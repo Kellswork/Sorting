3b35d1787da6a6bb0ca1dc0bb7feff91e2d8bcb7
kellswork <Bighead93+>
2019-10-08 14:30:10 +0100

    update bubble sort code

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -26,9 +26,7 @@ def bubble_sort(arr):
     while True:
         for i in range(0, len(arr) - 1):
             if arr[i] > arr[i + 1]:
-                temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
         if sorted(arr) == arr:
             return arr
